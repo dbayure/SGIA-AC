9ea03b8105ad14be503c5c96b89f725d6b27800c
dario <dario@dario-UX32LA>
2014-11-29 13:53:58 -0200

    factor modificado para subir

--- SGIA/ejb/src/main/java/uy/com/ceoyphoibe/SGIA/model/Factor.java
+++ SGIA/ejb/src/main/java/uy/com/ceoyphoibe/SGIA/model/Factor.java
@@ -65,49 +65,6 @@ public class Factor implements Serializable {
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + activoSistema;
-		result = prime * result + deAvance;
-		result = prime * result + estadado;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Factor other = (Factor) obj;
-		if (activoSistema != other.activoSistema)
-			return false;
-		if (deAvance != other.deAvance)
-			return false;
-		if (estadado != other.estadado)
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (nombre == null) {
-			if (other.nombre != null)
-				return false;
-		} else if (!nombre.equals(other.nombre))
-			return false;
-		return true;
-	}
-	@Override
-	public String toString() {
-		return "GrupoActuadores [id=" + id + ", nombre=" + nombre
-				+ ", estadado=" + estadado + ", deAvance=" + deAvance
-				+ ", activoSistema=" + activoSistema + "]";
-	}
+	
 	
 }
