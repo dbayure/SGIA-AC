dddaeb3f008030a90e5a4088b3c64e89b296e4a4
dario <dario@dario-UX32LA>
2014-12-20 11:55:42 -0200

    Lectura factor

---
+++ SGIA/ejb/src/main/java/uy/com/ceoyphoibe/SGIA/DTO/ResultadoAccion.java
@@ -0,0 +1,40 @@
+package uy.com.ceoyphoibe.SGIA.DTO;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import uy.com.ceoyphoibe.SGIA.model.Mensaje;
+
+public class ResultadoAccion implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 440338520655393531L;
+	
+	private Mensaje mensaje;
+	private Date fecha;
+	private String accion;
+	public Mensaje getMensaje() {
+		return mensaje;
+	}
+	public void setMensaje(Mensaje mensaje) {
+		this.mensaje = mensaje;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public String getAccion() {
+		return accion;
+	}
+	public void setAccion(String accion) {
+		this.accion = accion;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
