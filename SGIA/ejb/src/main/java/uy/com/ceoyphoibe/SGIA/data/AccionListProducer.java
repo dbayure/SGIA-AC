ce6d716850f77126438c9e7c0f711e13888499be
edurs <edurs47@gmail.com>
2015-01-07 20:01:46 -0200

    acciones ok

---
+++ SGIA/ejb/src/main/java/uy/com/ceoyphoibe/SGIA/data/AccionListProducer.java
@@ -0,0 +1,80 @@
+package uy.com.ceoyphoibe.SGIA.data;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.event.Observes;
+import javax.enterprise.event.Reception;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import uy.com.ceoyphoibe.SGIA.model.Accion;
+
+
+@RequestScoped
+public class AccionListProducer {
+	
+   @Inject
+   private EntityManager em;
+
+   private List<Accion> acciones;
+   
+   private List<Accion> accionesActuador;
+
+
+   @Produces
+   @Named
+   public List<Accion> getAcciones() {
+      return acciones;
+   }
+   
+   public List<Accion> getAccionesIdDispositivo(Long idDispositivo) 
+   {
+	   	 CriteriaBuilder cb = em.getCriteriaBuilder();
+		 CriteriaQuery<Accion> criteria = cb.createQuery(Accion.class);
+		 Root<Accion> accion = criteria.from(Accion.class);
+		 criteria.select(accion).orderBy(cb.asc(accion.get("fechaHora")));
+		 criteria.where(cb.equal(accion.get("idDispositivo"), idDispositivo));
+		 accionesActuador = em.createQuery(criteria).getResultList();
+		 return accionesActuador;
+	}
+   
+   public List<Accion> getAccionesIdDispositivoEntreFechas(Long idDispositivo, Timestamp min, Timestamp max) 
+   {
+	   	 CriteriaBuilder cb = em.getCriteriaBuilder();
+		 CriteriaQuery<Accion> criteria = cb.createQuery(Accion.class);
+		 Root<Accion> accion = criteria.from(Accion.class);
+		 criteria.select(accion).orderBy(cb.asc(accion.get("fechaHora")));
+ 
+		 Expression<Timestamp> fecha = accion.get("fechaHora");
+//		 Expression<Timestamp> fechaParam = cb.parameter(Timestamp.class);
+		 
+		 Predicate p= cb.between(fecha, min, max);
+		 criteria.where (cb.equal(accion.get("idDispositivo"), idDispositivo), p);
+						
+		 accionesActuador = em.createQuery(criteria).getResultList();
+		 return accionesActuador;
+	}
+
+   public void onListChanged(@Observes(notifyObserver = Reception.IF_EXISTS) final Accion accion) {
+	      retrieveAllOrderedByName();
+   }
+
+   @PostConstruct
+   public void retrieveAllOrderedByName() {
+      CriteriaBuilder cb = em.getCriteriaBuilder();
+      CriteriaQuery<Accion> criteria = cb.createQuery(Accion.class);
+      Root<Accion> accion = criteria.from(Accion.class);
+      criteria.select(accion).orderBy(cb.asc(accion.get("fechaHora")));
+      accionesActuador = em.createQuery(criteria).getResultList();
+   }
+}
---
+++ SGIA/web/src/main/java/uy/com/ceoyphoibe/sgia/bean/AccionesBean.java
@@ -0,0 +1,277 @@
+package uy.com.ceoyphoibe.sgia.bean;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Inject;
+
+import uy.com.ceoyphoibe.SGIA.controller.RegistroDispositivo;
+import uy.com.ceoyphoibe.SGIA.controller.RegistroGrupoActuadores;
+import uy.com.ceoyphoibe.SGIA.data.AccionListProducer;
+import uy.com.ceoyphoibe.SGIA.model.Accion;
+import uy.com.ceoyphoibe.SGIA.model.Actuador;
+import uy.com.ceoyphoibe.SGIA.model.ActuadorAvance;
+import uy.com.ceoyphoibe.SGIA.model.Dispositivo;
+import uy.com.ceoyphoibe.SGIA.model.GrupoActuadores;
+
+import com.lowagie.text.BadElementException;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Image;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.servlet.ServletContext;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "accionesBean")
+@SessionScoped
+public class AccionesBean {
+
+	private GrupoActuadores grupoActuadoresTemp;
+	private List<Long> listaIdActuadores;
+	private int idActuadorSeleccionado;
+	private long idGrupo;
+	private List<Accion> listaAcciones= new ArrayList<Accion>();
+	private Date fechaMin;
+	private Date fechaMax;
+	private Dispositivo dispositivoTemp;
+
+	
+	@Inject
+	private  AccionListProducer accionListProducer;
+	
+	@Inject
+	private  RegistroGrupoActuadores registroGrupoActuadores;
+	
+	@Inject
+	private  RegistroDispositivo registroDispositivo;
+
+	/**
+	 * @return the grupoActuadoresTemp
+	 */
+	public GrupoActuadores getGrupoActuadoresTemp() {
+		return grupoActuadoresTemp;
+	}
+
+	/**
+	 * @param grupoActuadoresTemp the grupoActuadoresTemp to set
+	 */
+	public void setGrupoActuadoresTemp(GrupoActuadores grupoActuadoresTemp) {
+		this.grupoActuadoresTemp = grupoActuadoresTemp;
+	}
+
+	/**
+	 * @return the idActuadorSeleccionado
+	 */
+	public int getIdActuadorSeleccionado() {
+		return idActuadorSeleccionado;
+	}
+
+	/**
+	 * @param idActuadorSeleccionado the idActuadorSeleccionado to set
+	 */
+	public void setIdActuadorSeleccionado(int idActuadorSeleccionado) {
+		this.idActuadorSeleccionado = idActuadorSeleccionado;
+	}
+	
+	public void cambioGrupoActuadores ()
+	{
+		grupoActuadoresTemp= registroGrupoActuadores.obtenerGrupoPorId(idGrupo);
+	}
+
+	/**
+	 * @return the idGrupo
+	 */
+	public long getIdGrupo() {
+		return idGrupo;
+	}
+
+	/**
+	 * @param idGrupo the idGrupo to set
+	 */
+	public void setIdGrupo(long idGrupo) {
+		this.idGrupo = idGrupo;
+	}
+	
+	public void obtenerAcciones()
+	{
+		listaAcciones= new ArrayList<Accion>();
+		if (idActuadorSeleccionado != 0)
+		{
+			Long idAct= (long) idActuadorSeleccionado;
+			if (fechaMin == null && fechaMax == null)
+				listaAcciones= accionListProducer.getAccionesIdDispositivo(idAct);
+			else
+			{
+				if (fechaMax == null)
+					fechaMax= new Date();
+				if (fechaMin == null)
+				{
+					fechaMin= new Date();
+					fechaMin.setYear(fechaMin.getYear()-1);
+				}
+				Timestamp min= new Timestamp(fechaMin.getTime());
+				Timestamp max= new Timestamp(fechaMax.getTime());
+				listaAcciones= accionListProducer.getAccionesIdDispositivoEntreFechas(idAct, min, max);
+			}
+		}
+		else
+		{
+			listaIdActuadores= new ArrayList<Long>();
+			if (grupoActuadoresTemp.getDeAvance().equals("N"))
+			{
+				List<Actuador> listaActuadores= grupoActuadoresTemp.getActuadores(); 
+				for (Actuador a : listaActuadores)
+				{
+					listaIdActuadores.add(a.getId());
+				}
+			}
+			else
+			{
+				Set<ActuadorAvance> listaActuadoresAvance= grupoActuadoresTemp.getActuadoresAvance();
+				for (ActuadorAvance a: listaActuadoresAvance)
+				{
+					listaIdActuadores.add(a.getId());
+				}
+			}
+			for (Long i : listaIdActuadores)
+			{
+				List<Accion> accionesTemp= new ArrayList<Accion>();
+				if (fechaMin == null && fechaMax == null)
+					accionesTemp= accionListProducer.getAccionesIdDispositivo(i);
+				else
+				{
+					if (fechaMax == null)
+						fechaMax= new Date();
+					if (fechaMin == null)
+					{
+						fechaMin= new Date();
+						fechaMin.setYear(fechaMin.getYear()-1);
+					}
+					Timestamp min= new Timestamp(fechaMin.getTime());
+					Timestamp max= new Timestamp(fechaMax.getTime());
+					accionesTemp= accionListProducer.getAccionesIdDispositivoEntreFechas(i, min, max);
+				}
+				for (Accion acc : accionesTemp)
+				{
+					listaAcciones.add(acc);
+				}
+			}
+		}
+	}
+
+	/**
+	 * @return the listaAcciones
+	 */
+	public List<Accion> getListaAcciones() {
+		return listaAcciones;
+	}
+
+	/**
+	 * @param listaAcciones the listaAcciones to set
+	 */
+	public void setListaAcciones(List<Accion> listaAcciones) {
+		this.listaAcciones = listaAcciones;
+	}
+
+	/**
+	 * @return the listaIdActuadores
+	 */
+	public List<Long> getListaIdActuadores() {
+		return listaIdActuadores;
+	}
+
+	/**
+	 * @param listaIdActuadores the listaIdActuadores to set
+	 */
+	public void setListaIdActuadores(List<Long> listaIdActuadores) {
+		this.listaIdActuadores = listaIdActuadores;
+	}
+
+	/**
+	 * @return the fechaMin
+	 */
+	public Date getFechaMin() {
+		return fechaMin;
+	}
+
+	/**
+	 * @param fechaMin the fechaMin to set
+	 */
+	public void setFechaMin(Date fechaMin) {
+		this.fechaMin = fechaMin;
+	}
+
+	/**
+	 * @return the fechaMax
+	 */
+	public Date getFechaMax() {
+		return fechaMax;
+	}
+
+	/**
+	 * @param fechaMax the fechaMax to set
+	 */
+	public void setFechaMax(Date fechaMax) {
+		this.fechaMax = fechaMax;
+	}
+
+	/**
+	 * @return the dispositivoTemp
+	 */
+	public Dispositivo getDispositivoTemp() {
+		return dispositivoTemp;
+	}
+
+	/**
+	 * @param dispositivoTemp the dispositivoTemp to set
+	 */
+	public void setDispositivoTemp(Dispositivo dispositivoTemp) {
+		this.dispositivoTemp = dispositivoTemp;
+	}
+	
+	public void obtenerDispositivo(Long id)
+	{
+		dispositivoTemp= registroDispositivo.obtenerDispositivoPorId(id);
+	}
+	
+	public void preProcessPDF(Object document) throws IOException, BadElementException, DocumentException {
+        Document pdf = (Document) document;
+        pdf.open();
+        pdf.setPageSize(PageSize.A4);
+ 
+        ServletContext servletContext = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
+        String logo = servletContext.getRealPath("") + File.separator + "resources" + File.separator + "gfx" + File.separator + "C&P2.png";
+         
+        pdf.add(Image.getInstance(logo));
+        pdf.addTitle("Acciones");
+        pdf.add(new Paragraph("Grupo de Actuadores: "+ grupoActuadoresTemp.getNombre()));
+        if (idActuadorSeleccionado == 0)
+        	pdf.add(new Paragraph("Actuadores: Todos"));
+        else
+        {
+        	Dispositivo dispositivo= registroDispositivo.obtenerDispositivoPorId(idActuadorSeleccionado);
+        	pdf.add(new Paragraph("Actuador: "+ dispositivo.getNombre()));
+        }
+        if (fechaMin != null && fechaMax != null)
+        {
+        	Timestamp min= new Timestamp(fechaMin.getTime());
+			Timestamp max= new Timestamp(fechaMax.getTime());
+        	pdf.add(new Paragraph("Período: "+ min + " - "+ max));
+        }
+        pdf.add(new Paragraph(" "));
+        pdf.add(new Paragraph(" "));
+	
+    }
+	
+}
